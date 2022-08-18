4c1be2498118a6b4f4d0992bff0f6a46152ff61e
wing <zlcnkkm@21cn.com>
2022-08-18 18:00:32 +0800

    增加动态光效，测试ok；部分方法仍未完善。

--- include/lamp.h
+++ include/lamp.h
@@ -10,6 +10,7 @@
 
 #include "rmt_ws2812.h"
 #include "driver/ledc.h"
+#include "freertos/semphr.h"
 
 #ifndef CPC405x_EN_GPIO_NUM
 #error "CPC405x_EN_GPIO_NUM undefine!"
@@ -45,11 +46,37 @@ typedef struct
     struct
     {
         /// 除了34及以上的只读Pin, 其它都可以
-        gpio_num_t      Output_IO_Num;
+        gpio_num_t      output_io_num;
  
+        /// LEDC 使用高速或低速通道
+        ledc_mode_t     speed_mode;
+
+        /// LEDC 高速和低速各有4个定时器(0 - 3)
+        ledc_timer_t    timer_num;
+
+        /// LEDC 输出通道
+        ledc_channel_t  channel;
+
+        /// 使用哪个时钟源；只有LEDC_USE_RTC8M_CLK 可以在休眠的时候继续运行；可以简单使用LEDC_AUTO_CLK。
+        ledc_clk_cfg_t  clk_cfg;
+
+        /// PWM 信号的频率
+        uint32_t        freq_hz;
+
+        /// 占空比分辨率位宽
+        ledc_timer_bit_t duty_resolution;
     } cpc405x;
     
 } Lamp_Config_t;
+
+// /**
+//  * 色彩校正结构体
+//  */
+// typedef struct
+// {
+//     float b, g, r;
+// } Lamp_Correction_t ;
+
 
 /**
  * 灯带控制初始化
@@ -57,6 +84,12 @@ typedef struct
  * @return 返回ESP_OK 表示完成初始化。
  */
 esp_err_t Lamp_Init(Lamp_Config_t *config);
+
+// /**
+//  * 设置颜色校正值
+//  * @param corr 若传入的某个分量的值大于1.0，则强制为1.0 。
+//  */
+// void Lamp_Set_Correction(Lamp_Correction_t *corr);
 
 /**
  * 通过电源管理IC为LED供电
@@ -85,9 +118,119 @@ esp_err_t Lamp_Off(void);
 /**
  * 让全部ws2812显示同一个颜色
  *  
- * @param color
+ * @param color 这里只使用第一个元素，其余led全部复制此颜色。
  * @return 返回ESP_OK 表示设置完成。
  */
-esp_err_t Lamp_Set_Color(WS2812_COLOR_t *color);
+esp_err_t Lamp_Set_Single_Color(WS2812_COLOR_t *color);
+
+
+/**
+ * 动态光效帧的计数模式
+ */
+typedef enum {
+    LAMP_EFFECT_FRAME_MODE_FORWARD = 1,     // 递增：索引正向计数到最后一帧后，返回第一帧。
+    LAMP_EFFECT_FRAME_MODE_TO_AND_FRO,      // 往复：索引正向计数到最后一帧后，再反向计数回到第一帧。
+    LAMP_EFFECT_FRAME_MODE_SINGLE,          // 单次：到最后一帧后，灯光固定在最后一帧不再变化。frames 的最后建议添加一个值为LAMP_EFFECT_HOLDER_FRAME 的元素，以减少向ws2812输出波形，节约电量。
+} Lamp_Effect_Frame_Mode_t;
+
+/**
+ * LAMP_EFFECT_STATE_MODE_PAUSE_ADD_ELAPSED 模式下播放的方向
+ */
+typedef enum {
+    LAMP_EFFECT_FRAME_DIRECTION_FORWARD = 1,    // 正向
+    LAMP_EFFECT_FRAME_DIRECTION_REVERSE = -1,   // 反向
+} Lamp_Effect_Frame_Direction_t;
+
+/**
+ *  在进入Lamp_Effect_Start() 时从哪里开始
+ */
+typedef enum {
+    LAMP_EFFECT_STATE_MODE_RESTART,             // 重新开始
+    LAMP_EFFECT_STATE_MODE_PAUSE,               // 从之前暂停的帧开始
+    LAMP_EFFECT_STATE_MODE_PAUSE_ADD_ELAPSED,   // 从之前暂停的位置再加上暂停后经过的时间来计算现在开始的帧
+} Lamp_Effect_Start_Mode_t;
+
+/**
+ *  Lamp_Effect_t 结构体的frames 数组每个元素表示该帧图像的首地址偏移，
+ *  某些帧有时不需要刷新（仍然显示上一帧的图像），
+ *  这时可把该元素的值设置为LAMP_EFFECT_HOLDER_FRAME 表示这帧不需要
+ *  向ws2812 发送波形。
+ */
+#define LAMP_EFFECT_HOLDER_FRAME    0xFFFFFFFF
+
+/**
+ *  动态光效数据对象结构体
+ * 
+ *  光效的动态主要由帧构成，
+ *  frames 数组每个元素就是一每帧图像的首地址偏移，
+ *  每帧的图像（每个ws2812可理解为像素）储存在table 中，
+ *  光效线程以freq的频率逐帧刷新，
+ *  按照mode 指定的模式，
+ *  逐帧从frames 找到table 中对应的图像地址，
+ *  交给ws2812驱动输出图像。
+ * 
+ *  注意：
+ *      该对象虽然有一个互斥锁成员，
+ *      但仍需要用户手动取锁和还锁来保证线程安全。
+ *            
+ */
+typedef struct
+{
+    struct
+    {
+        uint16_t                    freq : 16;  // 刷新率(hz)
+        bool                        clipping:1; // 是否启用裁剪模式   
+        bool                        virginity:1;// 配合pauseTime成员使用，防止对象第一次使用时出现计算错误，一般初始化为true。
+    };
+    Lamp_Effect_Frame_Mode_t        frameMode;  // 帧计数模式
+    Lamp_Effect_Frame_Direction_t   direction;  // 往复模式的当前方向
+    uint32_t                        current;    // 当前播放帧
+    uint32_t                        a;          // 裁剪帧开头，不能大于b。
+    uint32_t                        b;          // 裁剪帧末尾，不能小于a。
+    Lamp_Effect_Start_Mode_t        startMode;  // 进入Lamp_Effect_Start()时从哪帧开始
+    TickType_t                      pauseTime;  // 暂停时的系统Tick
+    uint32_t                       *frames;     // 每帧的图像在table 中的偏移量
+    uint32_t                        length;     // frame的长度
+    WS2812_COLOR_t                 *table;      // 每帧颜色(图像)索引表
+    SemaphoreHandle_t               semaphore;  // 数据锁，初始化时若不分配锁对象，则必须设置为NULL（NULL的话Lamp_Effect_Start() 会给对象创建锁）；用户修改此对象时必须先xSemaphoreTake()取锁，修改完毕必须xSemaphoreGive()还锁。
+} Lamp_Effect_t;
+
+/**
+ *  启动动态光效
+ * 
+ *  光效的制作可参照Lamp_Effect_t 结构体的说明。
+ * 
+ * @param effect    注意：
+ *                      若之前用此方法启动某个光效对象，
+ *                      中途改变对象的freq成员，是不会更新刷新率的，
+ *                      要变更正在运行的光效的刷新率，
+ *                      只能改变freq后再Lamp_Effect_Start()一次。
+ * @return 返回ESP_OK 表示动态光效启动。
+ */
+esp_err_t Lamp_Effect_Start(Lamp_Effect_t *effect);
+
+/**
+ *  停止动态光效
+ * @return 返回ESP_OK 表示动态光效停止。
+ */
+esp_err_t Lamp_Effect_Stop(void);
+
+/**
+ * 封装WS2812_Fill_Buffer()方法，
+ * 在Lamp_Loop_Start()开启自动刷新后，
+ * 向缓冲区填充数据。
+ * 
+ * @param color
+ * @return 返回ESP_OK 表示填充完成。
+ */
+// esp_err_t Lamp_Fill_Buffer(WS2812_COLOR_t *color);
+
+/**
+ *  LEDC 占空比分辨率最大位宽计算工具
+ * @param config 除duty_resolution成员外，
+ *               cpc405x下其余的成员先设置好期待值后再调用本方法传入。
+ * @return 若返回0 则表示
+ */
+ledc_timer_bit_t Lamp_LEDC_Duty_Resolution_Tool(Lamp_Config_t *config);
 
 #endif
--- src/lamp.c
+++ src/lamp.c
@@ -2,7 +2,7 @@
  * @Author: wing zlcnkkm@21cn.com
  * @Date: 2021-09-05 01:27:23
  * @LastEditors: wing zlcnkkm@21cn.com
- * @LastEditTime: 2022-08-13 21:22:20
+ * @LastEditTime: 2022-08-18 17:30:18
  * @FilePath: /JoyShot/src/lamp.c
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -20,8 +20,11 @@ static const char *TAG = "Lamp";
 
 static bool _is_on = false;
 
+// static Lamp_Correction_t _gamma = {1.0, 1.0, 1.0};
+
 esp_err_t Lamp_Init(Lamp_Config_t *config) {
 	ESP_LOGI(TAG, "Init");
+	_is_on = false;
 
 	esp_err_t ret;
     // 使用LEDC 驱动CPC405x
@@ -49,16 +52,21 @@ esp_err_t Lamp_Init(Lamp_Config_t *config) {
 
 	ret = ledc_set_duty(CPC405X_PWM_SPEED_MODE, CPC405X_PWM_CHANNEL, 4095);
 	if (ret != ESP_OK) return ret;
-
-	_is_on = false;
 	
 
     // ESP_ERROR_CHECK(WS2812_Init(&config->ws2812));
     // vTaskDelay(config->ws2812.RES/ 1000 / portTICK_RATE_MS);
     // ESP_ERROR_CHECK(WS2812_Loop_Start(25));
 
-	return WS2812_Init((WS2812_CONFIG_t *)WS2812_DEFAULT_CONFIG());
+	WS2812_CONFIG_t ws2812_congif = WS2812_Default_Config(WS2812_DIN_GPIO_NUM, WS2812_COUNT);
+	return WS2812_Init(&ws2812_congif);
 }
+
+// void Lamp_Set_Correction(Lamp_Correction_t *corr) {
+// 	_gamma.b = corr->b > 1.0 ? 1.0 : corr->b;
+// 	_gamma.g = corr->g > 1.0 ? 1.0 : corr->g;
+// 	_gamma.r = corr->r > 1.0 ? 1.0 : corr->r;
+// }
 
 esp_err_t Lamp_On(uint32_t delay) {
 	esp_err_t ret;
@@ -76,17 +84,321 @@ esp_err_t Lamp_Off(void) {
 	return ledc_stop(CPC405X_PWM_SPEED_MODE, CPC405X_PWM_CHANNEL, 0/*设置为低电平*/);
 }
 
-esp_err_t Lamp_Set_Color(WS2812_COLOR_t *color) {
+// /**
+//  * 色彩校正
+//  * @param destin	指向校正后颜色数据的目标数组。
+//  * @param source	指向将要校正颜色源数据。
+//  * @param n			被校正的颜色数
+//  * @return {*}
+//  */
+// static void _color_correction(WS2812_COLOR_t *destin, WS2812_COLOR_t *source, uint32_t n) {
+// 	for (size_t i = 0; i < n; i++) {
+// 		destin[i].b = (uint8_t)((float)(source[i].b) * _gamma.b);
+// 		destin[i].g = (uint8_t)((float)(source[i].g) * _gamma.g);
+// 		destin[i].r = (uint8_t)((float)(source[i].r) * _gamma.r);
+// 	}
+// }
+
+esp_err_t Lamp_Set_Single_Color(WS2812_COLOR_t *color) {
 	esp_err_t ret = ESP_OK;
 
 	if (!_is_on) ret = Lamp_On(CPC405X_LDO_UP_DELAY);
 	if (ret != ESP_OK) return ret;
 
 	static WS2812_COLOR_t buf[WS2812_COUNT];
-	for (size_t i = 0; i < WS2812_COUNT; i++)
-	{
-		memcpy(buf + i, color, sizeof(WS2812_COLOR_t));
+	for (size_t i = 0; i < WS2812_COUNT; i++){
+		memcpy(buf + i, color, 1);
+	// 	_color_correction(buf + i, color, 1);
 	}
-	
+
 	return WS2812_Send_LEDs(buf, WS2812_COUNT);
+}
+
+// static TaskHandle_t 	_effect_task_handle = NULL;
+static Lamp_Effect_t   *_effect		= NULL;
+
+static void _effect_task(WS2812_Event_t val) {
+
+	switch (val) {
+		// 将要刷新
+		case WS2812_Event_Before_Refresh:
+			xSemaphoreTake(_effect->semaphore, portMAX_DELAY);
+			break;
+
+		// 刚刷新过
+		case WS2812_Event_After_Refresh:
+			_effect->pauseTime = xTaskGetTickCount();
+			_effect->virginity = false;
+			// 推进到下一帧
+			switch (_effect->frameMode) {
+				case LAMP_EFFECT_FRAME_MODE_FORWARD:
+					// 回到a帧
+					if (_effect->clipping && 
+						((_effect->current + 1) > _effect->b)) {
+
+							_effect->current = _effect->a;
+					} 
+					// 回到头帧
+					else if ((!_effect->clipping) && 
+						((_effect->current + 1) > (_effect->length - 1))) {
+							
+							_effect->current = 0;
+					}	
+					// 继续前进
+					else _effect->current ++;
+					break;
+
+				case LAMP_EFFECT_FRAME_MODE_TO_AND_FRO:
+					// 到达换向点就翻转方向
+					if (_effect->clipping) {
+						if ((_effect->direction == LAMP_EFFECT_FRAME_DIRECTION_FORWARD) &&
+							(_effect->current == _effect->b)) {
+								
+								_effect->direction = LAMP_EFFECT_FRAME_DIRECTION_REVERSE;
+						}
+						else if ((_effect->direction == LAMP_EFFECT_FRAME_DIRECTION_REVERSE) &&
+							(_effect->current == _effect->a)) {
+								
+								_effect->direction = LAMP_EFFECT_FRAME_DIRECTION_FORWARD;
+						}
+					} else {
+						if ((_effect->direction == LAMP_EFFECT_FRAME_DIRECTION_FORWARD) &&
+							(_effect->current == (_effect->length - 1))) {
+								
+								_effect->direction = LAMP_EFFECT_FRAME_DIRECTION_REVERSE;
+						}
+						else if ((_effect->direction == LAMP_EFFECT_FRAME_DIRECTION_REVERSE) &&
+							(_effect->current == 0)) {
+								
+								_effect->direction = LAMP_EFFECT_FRAME_DIRECTION_FORWARD;
+						}
+					}
+					_effect->current += _effect->direction; 
+					break;
+
+				case LAMP_EFFECT_FRAME_MODE_SINGLE:
+					// 若还没到达末尾帧就继续推进
+					if ((_effect->clipping && (_effect->current < _effect->b)) || 
+						((!_effect->clipping) && (_effect->current < (_effect->length - 1)))) {
+
+							_effect->current ++;
+					}
+					// 已到达末尾帧就一直停在末尾。
+					break;
+				
+				default:
+					ESP_LOGW(TAG, "Unknow Lamp_Effect_Frame_Mode_t : %d \n", _effect->frameMode);
+					break;
+			}
+			
+			// 若是占位帧侧不输出
+			if (_effect->frames[_effect->current] != LAMP_EFFECT_HOLDER_FRAME)
+				// 提前输出下一帧到缓冲区
+				ESP_ERROR_CHECK(WS2812_Fill_Buffer(_effect->table + _effect->frames[_effect->current], WS2812_COUNT));
+			xSemaphoreGive(_effect->semaphore);	
+			break;
+		
+		default:
+			ESP_LOGW(TAG, "Unknow WS2812_Event_t : %d \n", val);
+			break;
+	}
+
+}
+
+/**
+ *  由于LAMP_EFFECT_STATE_MODE_PAUSE_ADD_ELAPSED 模式
+ * 	计算从哪帧开始的过程代码比较长，
+ *  因此将该过程拆分出来以便阅读。
+ * 
+ * @return 当方法返回的时候，effect->current已经被设置好，
+ * 		   此返回值只是简单返回effect->current，不需要再赋值给effect->current。
+ */
+static uint32_t _pause_add_elapsed_mode(Lamp_Effect_t *effect) {
+	// 算出距离上次暂停应该经过了多少帧(大约)
+	TickType_t ticks = effect->virginity ? 0 : xTaskGetTickCount() - effect->pauseTime;
+	uint32_t ms = ( ( ((uint64_t) ticks) * 1000) / configTICK_RATE_HZ );
+	uint32_t elapsed = ms / (1000 / effect->freq);
+
+	// 根据不同的播放模式计算从哪帧开始
+	switch (effect->frameMode)
+	{
+	case LAMP_EFFECT_FRAME_MODE_FORWARD:
+		// 裁剪模式
+		if (effect->clipping) {
+			uint32_t len = effect->b - effect->a + 1;
+			effect->current = (((effect->current - effect->a)) % len) + effect->a;
+		}
+		// 完整模式 
+		else {
+			effect->current = (elapsed + effect->current) % (effect->length);
+		}
+		break;
+
+	case LAMP_EFFECT_FRAME_MODE_SINGLE:
+		// 裁剪模式
+		if (effect->clipping) {
+			if ((effect->current + elapsed) > effect->b)
+				effect->current = effect->b;
+			else
+				effect->current += elapsed; 
+		}
+		// 完整模式 
+		else {
+			if ((effect->current + elapsed) > (effect->length - 1))
+				effect->current = effect->length - 1;
+			else
+				effect->current += elapsed; 			
+		}
+		break;
+	
+	case LAMP_EFFECT_FRAME_MODE_TO_AND_FRO:
+		// 裁剪模式，原理同完整模式
+		if (effect->clipping) {
+			uint32_t len = effect->b - effect->a + 1;
+			// effect->current = ((elapsed + (effect->current - effect->a)) % len) + effect->a;
+			uint32_t double_length  = len > 1 ? 
+									  (len - 1/*减去尾帧*/) * 2 :
+									  1;
+
+			// 裁剪模式的当前位置先偏移到0开始算
+			uint32_t double_current = effect->direction == LAMP_EFFECT_FRAME_DIRECTION_FORWARD?
+									  effect->current - effect->a :
+									  double_length - (effect->current - effect->a);
+			// 算出双倍长度在偏移之后（从0开始）的位置
+			effect->current = (elapsed + double_current) % (double_length);
+
+			// 位置超过长度就表示在反向播放
+			if (effect->current >= (len -1)) {
+				effect->current = (len -1) - (effect->current - (len - 1));
+				effect->direction = LAMP_EFFECT_FRAME_DIRECTION_REVERSE;
+			} else {
+				effect->direction = LAMP_EFFECT_FRAME_DIRECTION_FORWARD;
+			}
+
+			// 偏移回裁剪模式的位置
+			effect->current += effect->a;
+		}
+		// 完整模式 
+		else {
+			// 往复模式的播放长度当作正向模式的2倍，
+			// 由于两方向的尾帧（反向的尾帧就是正向的头帧）不会连着播两次，
+			// 所以计算的时候去掉1帧。
+			uint32_t double_length  = effect->length > 1 ? 
+									  (effect->length - 1/*减去尾帧*/) * 2 :
+									  1;
+
+			uint32_t double_current = effect->direction == LAMP_EFFECT_FRAME_DIRECTION_FORWARD?
+									  effect->current :
+									  double_length - effect->current;
+			// 算出双倍长度之后的位置
+			effect->current = (elapsed + double_current) % (double_length);
+
+			// 位置超过长度就表示在反向播放
+			if (effect->current >= (effect->length -1)) {
+				effect->current = (effect->length -1) - (effect->current - (effect->length - 1));
+				effect->direction = LAMP_EFFECT_FRAME_DIRECTION_REVERSE;
+			} else {
+				effect->direction = LAMP_EFFECT_FRAME_DIRECTION_FORWARD;
+			}
+		}
+
+		break;
+	
+	default:
+		ESP_LOGW(TAG, "Unknow Lamp_Effect_Frame_Mode_t : %d \n", effect->frameMode);
+		break;
+	}
+	return effect->current;
+}
+
+esp_err_t Lamp_Effect_Start(Lamp_Effect_t *effect) {
+	ESP_ERROR_CHECK((effect->freq < 1) || (effect->freq > 1000));
+	ESP_ERROR_CHECK(effect->length < 1);
+	ESP_ERROR_CHECK((effect->clipping) && (effect->a > effect->b));
+	ESP_ERROR_CHECK(effect->frames == NULL);
+	ESP_ERROR_CHECK(effect->table == NULL);
+
+	// if (_effect_task_handle) Lamp_Effect_Stop();
+
+	// 取锁
+	if (effect->semaphore == NULL)
+		effect->semaphore = xSemaphoreCreateMutex();
+	if (effect->semaphore == NULL)
+		return ESP_FAIL;
+	xSemaphoreTake(effect->semaphore, portMAX_DELAY);
+
+	// 计算从哪帧开始
+	switch (effect->startMode)
+	{
+	case LAMP_EFFECT_STATE_MODE_RESTART:
+		effect->current = 0;
+		break;
+
+	case LAMP_EFFECT_STATE_MODE_PAUSE: break;
+	
+	case LAMP_EFFECT_STATE_MODE_PAUSE_ADD_ELAPSED:
+		_pause_add_elapsed_mode(effect);
+		break;
+	
+	default:
+		ESP_LOGW(TAG, "Unknow Lamp_Effect_Start_Mode_t : %d \n", effect->startMode);
+		break;
+	}
+	// 还锁
+	xSemaphoreGive(effect->semaphore);
+
+	// 打开电源
+	esp_err_t ret = ESP_OK;
+	if (!_is_on) ret = Lamp_On(CPC405X_LDO_UP_DELAY);
+	if (ret != ESP_OK) return ret;
+
+	//----------------------------------------------------
+	//-- 注意后面是操作内部的_effect对象，不是参数的 effect ！ --
+
+	// 等待一个正在输出的帧完成
+	xSemaphoreTake((_effect ? _effect : effect)->semaphore, portMAX_DELAY);
+
+	// 更新光效数据的指针
+	_effect = effect;
+
+	// 先输出第一帧到缓冲区
+	ESP_ERROR_CHECK(WS2812_Fill_Buffer(_effect->table + _effect->frames[_effect->current], WS2812_COUNT));
+
+	xSemaphoreGive(_effect->semaphore);
+
+	return WS2812_Loop_Start(_effect->freq, _effect_task);
+	// esp_err_t ret = WS2812_Loop_Start(_effect->freq, _effect_task);
+	// if (ret != ESP_OK) return ret;
+
+	// if (_effect_task_handle) {
+	// 	ret = xTaskCreate(_effect_task, "LampEffect", 1024, NULL, 10, &_effect_task_handle);
+
+	// }
+
+    // return ret == pdPASS ? ESP_OK : ESP_FAIL;
+}
+
+esp_err_t Lamp_Effect_Stop(void) {
+	// if (_effect_task_handle) {
+	// 	if (vTaskDelete(_effect_task_handle) != pdPASS)
+	// 		return ESP_FAIL;
+
+	// 	_effect_task_handle = NULL;
+	// 	_effect 	= NULL; 
+	// }
+	
+	Lamp_Off();
+	WS2812_Loop_Stop();
+
+	// 等待一个正在输出的帧完成
+	if (_effect) {
+		Lamp_Effect_t *tmp = _effect; // 暂时取代_effect以便在_effect = NULL后还锁
+		xSemaphoreTake(tmp->semaphore, portMAX_DELAY);
+		_effect->pauseTime = xTaskGetTickCount();
+		_effect = NULL; 
+		xSemaphoreGive(tmp->semaphore);
+	}
+
+
+	return ESP_OK;
 }
