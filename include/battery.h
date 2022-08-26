3ddaaa300ba190eb87ce80c3bf5de685b6913bc1
wing <zlcnkkm@21cn.com>
2022-08-26 21:42:47 +0800

    只定义了个改名宏

---
+++ include/battery.h
@@ -0,0 +1,14 @@
+/**
+ *  电池状态
+ *  
+ *  2022-08-18    wing    创建.
+ */
+
+#ifndef BATTERY_H
+#define BATTERY_H
+
+#include "cpc405x.h"
+
+#define Battery_Init(config)    CPC405x_Battery_Init(config)
+
+#endif
